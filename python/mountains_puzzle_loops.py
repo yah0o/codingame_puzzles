c9d8dc814f2e634664adca6112632db692fa2bc6
Aleksey Agafonov <a_agafonov@wargaming.net>
2021-02-05 14:35:09 +0300

    add 1st puzzle tasks

---
+++ python/mountains_puzzle_loops.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+# The while loop represents the game.
+# Each iteration represents a turn of the game
+# where you are given inputs (the heights of the mountains)
+# and where you have to print an output (the index of the mountain to fire on)
+# The inputs you are given are automatically updated according to your last actions.
+
+
+# game loop
+while True:
+        fire_index = 0
+        max_mountain_height = -1
+        for i in range(8):
+            mountain_h = int(input()) # represents the height of one mountain.
+            if mountain_h > max_mountain_height:
+                max_mountain_height = mountain_h
+                fire_index = i
+        # The index of the mountain to fire on.
+        print(fire_index)
